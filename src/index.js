b5d7f2fe83146c707c659d6f86935ede3650c665
Fide <fidel2702@gmail.com>
2021-12-13 11:21:48 -0300

    iniciamos firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKBBU16LIhsrrLmDeZKdAtkeTxaqawLVA',
+  authDomain: 'ccba-bc5f1.firebaseapp.com',
+  projectId: 'ccba-bc5f1',
+  storageBucket: 'ccba-bc5f1.appspot.com',
+  messagingSenderId: '487584352194',
+  appId: '1:487584352194:web:c9638b3c5cb44bfa8df85b',
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
